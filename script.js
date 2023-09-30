8dec7e04f9403a60c2fd102b3d491313d4c103aa
sebastianbraun96 <Sebastian.leo.braun@gmail.com>
2023-09-30 13:33:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -633,8 +633,11 @@ async function writeToPort(data) {
 }
 
 function incrementBusyCounter() {
+  document.getElementById("terminal").innerHTML += "<br>";
+  document.getElementById("terminal").innerHTML += busyCounter;
+
   busyCounter++;
-  if ((!runningMeasurement && busyCounter >= 15) || ((busyCounter >= ((measurementPeriod * 10) - 1)) && measurand != "double") || ((busyCounter >= ((measurementPeriod * 5) - 1)) && measurand == "double")) {
+  if ((!runningMeasurement && (busyCounter >= 15)) || ((busyCounter >= ((measurementPeriod * 10) - 1)) && measurand != "double") || ((busyCounter >= ((measurementPeriod * 5) - 1)) && measurand == "double")) {
     chanelBusy = false;
     busyCounter = 0;
     if (debug) {
